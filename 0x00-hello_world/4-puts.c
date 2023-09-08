16ab9766f7ec803cfb50383ef2511dcdcde0462c
Martin Okem Odachi <odachi054@gmail.com>
2023-09-07 20:43:37 -0700

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  *  main- This is the main function
  *
- * return: return 0 sucess
+ * Return: return 0 sucess
  **/
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle);
