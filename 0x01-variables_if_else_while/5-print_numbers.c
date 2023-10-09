0ac2b0f6f7e8e7ee008c0d2dd66079c90609f0af
Martin Okem Odachi <odachi054@gmail.com>
2023-10-09 10:17:58 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ * Return: zero for success
+ **/
+
+int main(void)
+{
+	char num;
+
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
