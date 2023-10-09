c0398332257f6d14652b781a66184036a6ee229f
Martin Okem Odachi <odachi054@gmail.com>
2023-10-09 10:04:22 -0700

    ommitting letter q and e from alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * Return: zero for success
+ **/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
