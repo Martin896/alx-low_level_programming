b1d9f900604e6a10c7023866e09e4971df46f301
Martin Okem Odachi <odachi054@gmail.com>
2023-11-01 09:32:11 -0700

    printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ * main - start of the code
+ * Return: Always zero
+ **/
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{		_putchar(i);
+	}
+}
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
