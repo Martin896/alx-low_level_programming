2b9d42b5180f04ab0822f7f25f498bd0fbd10c33
Martin Okem Odachi <odachi054@gmail.com>
2023-11-08 20:21:49 -0800

    modified code by adding if block

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_last_digit - function that prints the last digit of a number
- * @i: function argument- last digit of a number
- * Return: i
+ * print_last_digit - function prints the last digit of a number
+ * @i: function variable
+ * Return: last digit
  **/
 
 int print_last_digit(int i)
 {
-	i = i % 10;
+	int last_digit;
 
-	return (i);
+	last_digit = i % 10;
+	if (last_digit < 0)
+		last_digit = -last_digit;
+	_putchar('0' + last_digit);
+	return (last_digit);
+
 }
+
