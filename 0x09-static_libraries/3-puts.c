af60681b9777792b73187814547365b7ca2a8504
Martin Okem Odachi <odachi054@gmail.com>
2023-11-17 17:30:30 -0800

    Editted

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
-void _puts(char *str)
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -10,4 +10,20 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
+}
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,8 @@
-#ifndef
-#define
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
