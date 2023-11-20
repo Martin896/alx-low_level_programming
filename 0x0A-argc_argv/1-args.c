7fe0a9fbf51b9d9aa9e14fa665010db0a7705db7
Martin Okem Odachi <odachi054@gmail.com>
2023-11-20 13:50:30 -0800

    Dear Dr obed Thank you

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Start of the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void) argv;
+	return (0);
+
+}
