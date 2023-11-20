f36ef7613c2a219bc4654802ea83be04e0249c3a
Martin Okem Odachi <odachi054@gmail.com>
2023-11-20 13:36:56 -0800

    This argv  and arc c are very much intressant

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ * @argc: argument count
+ * @argv: argument array or vector
+ * Return: Always 0
+ **/
+
+int main(__attribute((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
