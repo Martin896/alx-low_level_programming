f84d652df651042c0953ee208786e6ae32a3c0a8
Martin Okem Odachi <odachi054@gmail.com>
2023-11-25 11:13:31 -0800

    modified

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,8 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
+	if (arraypt == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		arraypt[i] = c;
 	return (arraypt);
