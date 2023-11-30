37b5790968091bfaf7106890ef3893e411640578
Martin Okem Odachi <odachi054@gmail.com>
2023-11-29 18:57:02 -0800

    iterating an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - iterates over an array
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: function pointer
+ * Return: void
+ **/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size ; i++)
+		action(array[i]);
+}
