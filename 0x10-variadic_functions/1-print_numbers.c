d7624cdb512e31b005d49bcd70ed010e7e07edfb
Martin Okem Odachi <odachi054@gmail.com>
2023-11-30 17:41:38 -0800

    i did it again all by myself and chatgpt advise

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,29 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - function to print numbers
+ * @separator: string between numbers
+ * @n: count
+ * Return: Void
+ **/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			return;
+		printf("%d", va_arg(nums, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(nums);
+	printf("\n");
+}
