799dad0fab2ce5f7821fb56fac34b167b57f50f6
Martin Okem Odachi <odachi054@gmail.com>
2023-12-01 09:09:26 -0800

    update code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,49 +3,50 @@
 #include <stdio.h>
 
 /**
- * print_all - function that print any value
+ * print_all - function that prints anything
  * @format: constant character pointer
  * Return: void
  **/
 void print_all(const char * const format, ...)
 {
 	va_list args;
-
 	const char *ptr = format;
 	char *separator = "";
 	unsigned int i = 0;
+	char *str;
 
 	va_start(args, format);
 
 	while (format != NULL && *ptr != '\0')
 	{
-		if (*ptr == 'c')
-			printf("%s%c", separator, va_arg(args, int));
-		else if (*ptr == 'i')
-			printf("%s%d", separator, va_arg(args, int));
+		i = 0;
 
-		else if (*ptr == 'f')
-			printf("%s%f", separator, va_arg(args, double));
-
-		else if (*ptr == 's')
+		while (i < 4)
 		{
-			char *str = va_arg(args, char*);
-
-			if (str == NULL)
-				printf("%s(nil)", separator);
-			else
-
-				printf("%s%s", separator, str);
+			if (*ptr == "cifs"[i])
+			{
+				if (*ptr == 'c')
+					printf("%s%c", separator, va_arg(args, int));
+				else if (*ptr == 'i')
+					printf("%s%d", separator, va_arg(args, int));
+				else if (*ptr == 'f')
+					printf("%s%f", separator, va_arg(args, double));
+				else if (*ptr == 's')
+				{
+					str = va_arg(args, char *);
+					if (str == NULL)
+						printf("%s(nil)", separator);
+					else
+						printf("%s%s", separator, str);
+				}
+				separator = ", ";
+				break;
+			}
+			i++;
 		}
-
-		separator = ", ";
 		ptr++;
-		i++;
-
 	}
 
-
 	va_end(args);
-
 	printf("\n");
 }
