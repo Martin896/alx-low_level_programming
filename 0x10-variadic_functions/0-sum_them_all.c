db3983da0f026b5b82e2396dd9d03a6db913b20b
Martin Okem Odachi <odachi054@gmail.com>
2023-11-30 16:55:41 -0800

    My first alx variadic function and other files

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,3 +1,27 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
+/**
+ * sum_them_all - functtion that add all arguments passed
+ * @n: count argument
+ * Return: sum of numbers
+ **/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list them_all;
+	unsigned int i;
+	int result = 0;
+
+	va_start(them_all, n);
+
+	for (i =  0; i < n; i++)
+	{
+		if (n == 0)
+			return (0);
+		result = result + va_arg(them_all, int);
+	}
+	va_end(them_all);
+	return (result);
+}
 
